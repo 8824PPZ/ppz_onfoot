2a83355bdbcc49303a34356611dc27eadd34a4f6
ppz <ppzisgod@gmail.com>
2023-12-21 14:16:19 +0800

    更新mapbox token

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,6 +1,6 @@
 // const
 const MAPBOX_TOKEN =
-  'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
+  'pk.eyJ1IjoiYnVnMzY1IiwiYSI6ImNscHRnMnI0YjBicmUyaXF6Nzh3Y3djeGgifQ.Lp4Tg7ifvOI0NiyPzeVM4w';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
